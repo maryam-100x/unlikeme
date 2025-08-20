764f1f06d6d8c6df02cdd6439997c3221902c87f
solpotfun <209683647+solpotfun@users.noreply.github.com>
2025-08-20 19:03:15 +0200

    d

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const COIN_NAME = "Unlike me";
 export const TICKER = "$UNLIKEME";
 export const CONTRACT_ADDRESS = "Soon";
 
-export const X_COMMUNITY_URL = "https://x.com/";
+export const X_COMMUNITY_URL = "https://x.com/i/communities/1958210648464072723/";
 export const PUMPFUN_URL = `https://pump.fun/coin/${CONTRACT_ADDRESS}`;
 export const DEXSCREENER_URL = `https://dexscreener.com/solana/${CONTRACT_ADDRESS}`;
 
